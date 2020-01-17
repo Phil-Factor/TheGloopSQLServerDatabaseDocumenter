c77695fcdf936bf5ea7ee135fba0539c4d5fe033
Phil Factor <Phil.Factor@simple-talk.com>
2020-01-17 14:09:02 +0000

    minor update

--- ParseJSONMetadataToUpdateTheDocumentation.sql
+++ ParseJSONMetadataToUpdateTheDocumentation.sql
@@ -146,6 +146,7 @@ WHILE @ii<=@iiMax
     END
 SELECT @NumberChanged= @changed 
 GO 
+
 USE customers
 DECLARE @JSON NVARCHAR(MAX);
 SELECT @JSON = BulkColumn
@@ -155,3 +156,105 @@ SELECT @JSON = BulkColumn
 DECLARE @howManyChanges INT;
 EXECUTE #ParseJSONMetadataToUpdateTheDocumentation @JSON, @howManyChanges OUTPUT;
 SELECT Convert(VARCHAR(5),@howManyChanges)+ ' descriptions were either changed or added' AS ResultOfUpdate
+
+
+DECLARE @JSON NVARCHAR(MAX);
+SELECT @JSON = BulkColumn
+  FROM
+  OpenRowset(BULK 'D:\Data\RawData\customers-ColObs.json', SINGLE_NCLOB)
+  AS MyJSONFile;
+
+DROP TABLE IF EXISTS #TheObjects;
+CREATE TABLE #TheObjects
+  (
+  Name sysname NOT NULL,
+  Type NVARCHAR(30) NOT NULL,
+  Description NVARCHAR(3750) NULL,
+  ParentName sysname NULL,
+  [Contains] NVARCHAR(MAX) NULL
+  );
+
+INSERT INTO #TheObjects (Name, Type, Description, ParentName, [Contains])
+  SELECT BaseObjects.Name, BaseObjects.Type, BaseObjects.Description, NULL,
+    [Contains]
+    FROM
+    OpenJson(@JSON)
+    WITH
+      (
+      Name NVARCHAR(80) '$.Name', Type NVARCHAR(80) '$.Type',
+      Parent NVARCHAR(80) '$.Parent',
+      Description NVARCHAR(MAX) '$.Description',
+      [Contains] NVARCHAR(MAX) '$.contains' AS JSON
+      ) AS BaseObjects;
+
+INSERT INTO #TheObjects (Name, Type, Description, ParentName, [Contains])
+  SELECT objvalues.Name, obj.[Key] AS Type, objvalues.Description,
+    #TheObjects.Name AS ParentName, NULL AS [contains]
+    FROM #TheObjects
+      OUTER APPLY OpenJson(#TheObjects.[Contains]) AS child
+      OUTER APPLY OpenJson(child.Value) AS obj
+      OUTER APPLY
+    OpenJson(obj.Value)
+    WITH (Name NVARCHAR(80) '$.Name', Description NVARCHAR(MAX) '$.Description') AS objvalues;
+
+
+DROP TABLE IF EXISTS #EPParentObjects;
+CREATE TABLE #EPParentObjects
+  (
+  TheOneToDo INT IDENTITY(1, 1),
+  level0_type VARCHAR(128) NULL,
+  level0_Name sysname NULL,
+  level1_type VARCHAR(128) NULL,
+  level1_Name sysname NULL,
+  level2_type VARCHAR(128) NULL,
+  level2_Name sysname NULL,
+  [Description] NVARCHAR(3750),
+  );
+
+INSERT INTO #EPParentObjects
+  (level0_type, level0_Name, level1_type, level1_Name, level2_type,
+level2_Name, Description)
+  SELECT 'schema' AS level0_type, ParseName(Name, 2) AS level0_Name,
+      CASE WHEN Type LIKE '%FUNCTION%' THEN 'FUNCTION'
+        WHEN Type LIKE '%TABLE%' THEN 'TABLE'
+        WHEN Type LIKE '%PROCEDURE%' THEN 'PROCEDURE'
+        WHEN Type LIKE '%RULE%' THEN 'RULE'
+        WHEN Type LIKE '%VIEW%' THEN 'VIEW'
+        WHEN Type LIKE '%DEFAULT%' THEN 'DEFAULT'
+        WHEN Type LIKE '%AGGREGATE%' THEN 'AGGREGATE'
+        WHEN Type LIKE '%LOGICAL FILE NAME%' THEN 'LOGICAL FILE NAME'
+        WHEN Type LIKE '%QUEUE%' THEN 'QUEUE'
+        WHEN Type LIKE '%RULE%' THEN 'RULE'
+        WHEN Type LIKE '%SYNONYM%' THEN 'SYNONYM'
+        WHEN Type LIKE '%TYPE%' THEN 'TYPE'
+        WHEN Type LIKE '%XML SCHEMA COLLECTION%' THEN 'XML SCHEMA COLLECTION' 
+	    ELSE'UNKNOWN' 
+	  END AS level1_type,
+    ParseName(Name, 1) AS level1_Name, NULL AS level2_type,
+    NULL AS level2_Name, Description
+    FROM #TheObjects
+    WHERE ParentName IS NULL;
+
+INSERT INTO #EPParentObjects
+  (level0_type, level0_Name, level1_type, level1_Name, level2_type,
+level2_Name, Description)
+  SELECT level0_type, level0_Name, level1_type, level1_Name,
+      CASE WHEN Type LIKE '%COLUMN%' THEN 'COLUMN'
+        WHEN Type LIKE '%CONSTRAINT%' THEN 'CONSTRAINT'
+        WHEN Type LIKE '%EVENT NOTIFICATION%' THEN 'EVENT NOTIFICATION'
+        WHEN Type LIKE '%INDEX%' THEN 'INDEX'
+        WHEN Type LIKE '%PARAMETER%' THEN 'PARAMETER'
+        WHEN Type LIKE '%TRIGGER%' THEN 'TRIGGER' 
+		ELSE 'UNKNOWN' 
+	  END AS Level2_type,
+    #TheObjects.Name AS Level2_name, #TheObjects.Description
+    FROM #EPParentObjects
+      INNER JOIN #TheObjects
+        ON level1_Name = ParseName(ParentName, 1) 
+		  AND level0_Name =ParseName(ParentName, 2);
+
+SELECT * FROM #EPParentObjects AS EPO
+
+
+
+
